53796b1d2897c7348bcd976a7bb5fa388544b425
YanchukViktoria <139624726+YanchukViktoria@users.noreply.github.com>
2024-11-15 18:35:58 +0200

    f

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let inp = document.querySelector("#inp");
+let btn = document.querySelector("#btn");
+
+inp.value = "text";
+btn.textContent = "Submit";
+
+let img = document.querySelector("img");
+
+img.setAttribute("src", "./giff.gif")
+img.setAttribute("alt", "paw");
+
+let li = document.querySelector("ul li").textContent = "firstg";
